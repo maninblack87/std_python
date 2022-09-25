d69695d7ef10de6a6765c038a3151ddbdc38ca3d
maninblack87 <maninblack87@naver.com>
2022-09-26 00:38:53 +0900

    220926 first commit

--- GUI/12.py
+++ GUI/12.py
@@ -7,11 +7,16 @@ root.geometry("640x480")
 frame = Frame(root)
 frame.pack()
 
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side="right")
+
 # 주의: set이 없으면 스크롤을 내려도 다시 올라옴
 listbox = Listbox(frame, selectmode="extended",
                   height=10, yscrollcommand=scrollbar.set)
 for i in range(1, 32):  # 1~31일
     listbox.insert(END, str(i)+"일")
 listbox.pack(side="left")
+
+scrollbar.config(command=listbox.yview)
 
 root.mainloop()
